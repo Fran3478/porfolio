8bfaedf8402b7ecd4eb00d4759f008b70c4f8a91
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-02-07 11:04:24 -0300

    quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Sidenav from './components/Sidenav'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
 
 function App() {
 
   return (
     <>
       <div>
-
+        <Sidenav />
+        <Home />
+        <AboutMe />
       </div>
     </>
   )
