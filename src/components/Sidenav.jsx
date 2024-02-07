4dc841a75f4756b0d19d8a43c04c341b793f5325
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-02-07 11:26:03 -0300

    add feature close sidebar on button click

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -14,30 +14,30 @@ const Sidenav = () => {
                 !nav ? (
                     <AiOutlineMenu onClick={handleNav} className='absolute top-4 left-4 z-[99] md:hidden' color='white' />
                 ) : (
-                    <AiOutlineClose onClick={handleNav} className='absolute top-4 left-4 z-[99] md:hidden' color='white' />
+                    <AiOutlineClose onClick={handleNav} className='fixed top-4 left-4 z-[99] md:hidden' color='white' />
                 )
             }
 
             {
                 nav ? (
-                    <div className='fixed w-full h-screen bg-[#062446] flex flex-col justify-center items-center z-20 md:hidden'>
-                        <a href='#main' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200'>
+                    <div className='fixed w-full h-screen bg-[#062446] flex flex-col justify-center items-center z-20 md:hidden backdrop-blur-sm'>
+                        <a href='#main' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200' onClick={handleNav}>
                             <AiOutlineHome size={20} />
                             <span className='pl-4'>Home</span>
                         </a>
-                        <a href='#aboutMe' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200'>
+                        <a href='#aboutMe' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200' onClick={handleNav}>
                             <AiOutlineUser size={20} />
                             <span className='pl-4'>About Me</span>
                         </a>
-                        <a href='#projects' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200'>
+                        <a href='#projects' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200' onClick={handleNav}>
                             <AiOutlineProject size={20} />
                             <span className='pl-4'>Projects</span>
                         </a>
-                        <a href='#contact' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200'>
+                        <a href='#contact' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200' onClick={handleNav}>
                             <AiOutlineMail size={20} />
                             <span className='pl-4'>Contact</span>
                         </a>
-                        <a href='#resume' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200'>
+                        <a href='#resume' className='w-[70%] sm:w-80 flex justify-center items-center rounded-full shadow-md shadow-slate-600 bg-[#51A6F0] text-white m-2 p-4 cursor-pointer active:scale-110 ease-in duration-200' onClick={handleNav}>
                             <LuFileDown size={20} />
                             <span className='pl-4'>Resume</span>
                         </a>
