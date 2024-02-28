dba176abc7e0f44950035cae8d83ce5dabb5c9bf
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-02-28 10:08:31 -0300

    modif colors && upgrade responsive

--- src/components/navbar/SideNavbar.jsx
+++ src/components/navbar/SideNavbar.jsx
@@ -5,47 +5,23 @@ const SideNavbar = () => {
     return (
         <div className="hidden w-[4rem] h-screen right-0 fixed z-50 md:flex flex-col mr-[2rem]">
             <div className="m-auto text-white text-2xl">
-                <a href="#home" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 transition-all duration-300'>
+                <a href="#home" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 hover:brightness-150 transition-all duration-300'>
                     <AiOutlineHome />
                 </a>
-                <a href="#aboutMe" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 transition-all duration-300'>
+                <a href="#aboutMe" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 hover:brightness-150 transition-all duration-300'>
                     <AiOutlineUser />
                 </a>
-                <a href="#projects" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 transition-all duration-300'>
+                <a href="#projects" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 hover:brightness-150 transition-all duration-300'>
                     <AiOutlineProject />
                 </a>
-                <a href="#contact" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 transition-all duration-300'>
+                <a href="#contact" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 hover:brightness-150 transition-all duration-300'>
                     <AiOutlineMail />
                 </a>
-                <a href="#resume" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 transition-all duration-300'>
+                <a href="#resume" className='bg-[#062446] rounded-full w-fit flex m-2 p-4 cursor-pointer hover:scale-125 hover:brightness-150 transition-all duration-300'>
                     <LuFileDown />
                 </a>
             </div>
         </div>
-        // <div className='md:block hidden fixed right-5 top-[25%] z-10'>
-        //     <div className='flex flex-col items-end'>
-        //         <a href="#home" className='group rounded-full flex justify-end items-center shadow-md shadow-slate-600 bg-[#062446] m-2 p-4 cursor-pointer hover:scale-x-100'>
-        //             <span className='hidden group-hover:block pr-4 text-white transform transition-transform duration-300 delay-150'>Home</span>
-        //             
-        //         </a>
-        //         <a href="#aboutMe" className='group rounded-full flex justify-end items-center shadow-md shadow-slate-600 bg-[#062446] m-2 p-4 cursor-pointer transform transition-transform duration-400 hover:scale-x-100'>
-        //             <span className='hidden group-hover:block pr-4 text-white transform transition-transform duration-300 delay-150'>About me</span>
-        //             <AiOutlineUser size={20} color='white' />
-        //         </a>
-        //         <a href="#projects" className='group rounded-full flex justify-end items-center shadow-md shadow-slate-600 bg-[#062446] m-2 p-4 cursor-pointer transform transition-transform duration-300 hover:scale-x-100'>
-        //             <span className='hidden group-hover:block pr-4 text-white transform transition-transform duration-300 delay-150'>Projects</span>
-        //             
-        //         </a>
-        //         <a href="#contact" className='group rounded-full flex justify-end items-center shadow-md shadow-slate-600 bg-[#062446] m-2 p-4 cursor-pointer transform transition-transform duration-300 hover:scale-x-100'>
-        //             <span className='hidden group-hover:inline-block pr-4 text-white transform transition-transform duration-300 delay-150'>Contact</span>
-        //             
-        //         </a>
-        //         <a href="#resume" className='group rounded-full flex justify-end items-center shadow-md shadow-slate-600 bg-[#062446] m-2 p-4 cursor-pointer transform transition-transform duration-300 hover:scale-x-100'>
-        //             <span className='hidden group-hover:block pr-4 text-white transform transition-transform duration-300 delay-150'>Resume</span>
-        //             
-        //         </a>
-        //     </div>
-        // </div>
     )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,9 +3,9 @@ import Container from "./Container"
 const Projects = () => {
 
     return (
-        <div id="projects" className="min-h-screen max-h-fit h-full max-w-screen w-full bg-[#51A6F0] relative text-center sm:text-left">
+        <div id="projects" className="min-h-screen max-h-fit h-full max-w-screen w-full bg-[#51a6f0ab] relative text-center sm:text-left">
             <div className="mx-[2rem] sm:ml-[3.8rem] sm:mr-[3.8rem] md:ml-16 md:mr-[8rem]">
-                <h1 className="text-2xl md:text-3xl lg:text-5xl text-[#24ffab] font-extrabold py-10">Projects</h1>
+                <h1 className="text-2xl md:text-3xl lg:text-5xl text-[#155561] font-extrabold py-10">Projects</h1>
                 <div className="flex justify-center pb-10">
                     <Container />
                 </div>
