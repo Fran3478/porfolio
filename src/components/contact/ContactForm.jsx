55de0b2d97fe8261b2e96c9b9fc9f5df93e8dd75
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-03-18 14:47:00 -0300

    fix

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -62,7 +62,7 @@ const ContactForm = () => {
             }
         ]
         const passCheck = bulkCheck(fields)
-        const text = `New contact!\nName: ${name || "not provided"}\nEmail: ${email}\nMessage: ${message}`
+        const text = `New contact!<br/>Name: ${name || "not provided"}<br/>Email: ${email}<br/>Message: ${message}`
         if (passCheck) {
             axios.get(`https://api.telegram.org/bot${import.meta.env.VITE_BOT_TOKEN}/sendMessage?chat_id=${import.meta.env.VITE_CHAT_ID}&text=${text}`)
                 .then(response => {
