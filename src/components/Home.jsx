b0022f03313f15ff9fda8ebe386e7ce2197b9202
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-03-19 13:40:59 -0300

    fix img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
       </div>
       <div className="flex md:flex-col justify-center md:justify-normal bg-transparent w-full h-1/3 md:h-full md:w-1/3 relative select-none">
         <div className="md:absolute md:inset-x-10 md:inset-y-0 bg-[#51A6F0] w-[190%] md:skew-x-[-20deg] md:origin-center"></div>
-        <div className="absolute w-[12rem] h-[12rem] lg:w-[17rem] xl:w-[20rem] lg:h-[17rem] xl:h-[20rem] aspect-square -top-1/4 sm:-top-20 md:-left-[15%] md:top-[calc(50%-10rem)] rounded-full border-8 border-[#65a3f2] bg-[#416B9A] overflow-hidden">
-          <img className="pointer-events-none object-cover" src="https://res.cloudinary.com/dacbde3so/image/upload/v1707321696/portfolio-img/qwcbzj4yuthlx6xkdmni.png" alt="photo" />
+        <div className="absolute -top-1/4 sm:-top-20 md:-left-[15%] md:top-[calc(50%-10rem)] rounded-full border-8 border-[#65a3f2] bg-[#416B9A] overflow-hidden">
+          <img className="pointer-events-none w-[12rem] h-[12rem] lg:w-[17rem] xl:w-[20rem] lg:h-[17rem] xl:h-[20rem] aspect-square object-cover" src="https://res.cloudinary.com/dacbde3so/image/upload/v1710809019/portfolio-img/jhxhwyvlwl73to5ny4la.webp" alt="photo" />
         </div>
       </div>
     </div>
