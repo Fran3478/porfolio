8886b4f3a28e6fcc06a680c7276af4a0beb7b159
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-04-03 15:52:39 -0300

    fix button

--- src/components/buttons/ProjectButton.jsx
+++ src/components/buttons/ProjectButton.jsx
@@ -4,7 +4,7 @@ const ProjectButton = ({ url, ico }) => {
     const displayButton = url ?
         <a className="bg-[#51A6F0] rounded-full text-2xl md:text-3xl lg:text-5xl p-2 hover:scale-110 hover:contrast-125 ease-in-out transition-all duration-300" href={url} target="_blank" rel="noreferrer">{ico}</a>
         :
-        <p className="bg-[#a1b1c0] rounded-full text-2xl md:text-3xl lg:text-5xl p-2">{ico}</p>
+        <p className="bg-[#a1b1c0] rounded-full text-2xl md:text-3xl lg:text-5xl p-2 cursor-not-allowed">{ico}</p>
     return (
         displayButton
     )
