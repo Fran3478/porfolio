b3662b5705f93711e03d1acdfc8b72c8e75c1c07
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2024-04-05 12:12:35 -0300

    fix status bar internal distribution

--- src/components/projects/Status.jsx
+++ src/components/projects/Status.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types"
 
 const Status = ({ status }) => {
     return (
-        <div className={`${status.control === "finished" ? "bg-green-600 text-green-200" : status.control === "on development" && "bg-yellow-500 text-yellow-100"} rounded-full w-fit grid grid-cols-5 my-2 mr-3 self-end py-[0.2rem] px-[0.6rem]`}>
-            <div className={`${status.control === "finished" ? "bg-green-200" : status.control === "on development" && "bg-yellow-100"} h-2 w-2 rounded-full m-auto`} />
-            <p className="col-span-4 col-start-2 text-sm font-medium">{status.title}</p>
+        <div className={`${status.control === "finished" ? "bg-green-600 text-green-200" : status.control === "on development" && "bg-yellow-500 text-yellow-100"} rounded-full w-fit flex my-2 mr-3 self-end py-[0.2rem]`}>
+            <div className={`${status.control === "finished" ? "bg-green-200" : status.control === "on development" && "bg-yellow-100"} h-2 w-2 rounded-full m-auto mx-[0.4rem]`} />
+            <p className="col-span-4 col-start-2 text-sm mr-[0.6rem]">{status.title}</p>
         </div>
     )
 }
